4feab1db663e29129954ebb46560dccdab6ba0b6
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-15 10:04:39 +0100

    the text that will be displayed

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const StateManagementApp(title: 'Random Color App'),
+      home: const StateManagementApp(title: 'Random Color App', color: Colors.indigo),
     );
   }
 }
 
 class StateManagementApp extends StatefulWidget {
-  const StateManagementApp({Key? key, required this.title}) : super(key: key);
+  const StateManagementApp({Key? key, required this.title, required this.color}) : super(key: key);
 
   final String title;
+  final Color color;
 
   @override
   State<StateManagementApp> createState() => _StateManagementAppState();
@@ -35,9 +36,30 @@ class _StateManagementAppState extends State<StateManagementApp> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        backgroundColor: widget.color,
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('This color displayed is :'),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              height: MediaQuery.of(context).size.height * 0.13,
+              color: widget.color,
+              child: const Center(
+                child: Text(
+                  'R(), G(), B()',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
